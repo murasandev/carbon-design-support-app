8fbb0eb531433e9917cec77e29f013f4b01f0f08
murasandev <dmurayama93@gmail.com>
2024-01-17 22:46:41 -1000

    move images folder to public folder, and set and style image containers

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 'use client'
-import map from '../images/locationsimage.png'
+// import map from '../public/images/locationsimage.png'
 import styles from './page.module.css'
 import Profile from '@/components/Profile'
-import { Accordion,AccordionItem, AccordionSkeleton, Button, ActionableNotification, ContentSwitcher, Switch } from '@carbon/react'
+import { Accordion,AccordionItem, AccordionSkeleton, Button, ActionableNotification, ContentSwitcher, Switch, ToastNotification } from '@carbon/react'
 import Sidebar from '@/components/Sidebar'
+// import honolulu from '../public/images/Honolulu.jpeg'
 
 export default function Home() {
   return (
@@ -20,22 +21,19 @@ export default function Home() {
             </Accordion>
 
             <Button kind="danger">Contact Us</Button>
-
-            <ActionableNotification
-              actionButtonLabel="Action"
-              aria-label="closes notification"
-              kind="error"
-              onActionButtonClick={function noRefCheck(){}}
-              onClose={function noRefCheck(){}}
-              onCloseButtonClick={function noRefCheck(){}}
-              statusIconDescription="notification"
-              subtitle="Subtitle text goes here"
-              title="Notification title"
-            />
           </div>
           <div className={styles.switcherHolder}>
+            <div className={styles.columnHolder}>
+              <div className={styles.columnLeft}>
+                {/* <img src='./images/Honolulu.jpeg' alt='hnl.jpg' width='100%' height='auto'></img> */}
+                <h1>Honolulu</h1>
+              </div>
+              <div className={styles.columnRight}>
+                <h1>London</h1>
+              </div>
+            </div>
             {/* put map here */}
-            <div className={styles.mapHolder}></div>
+            {/* <div className={styles.mapHolder}></div> */}
             <ContentSwitcher className={styles.switcher}
               onChange={(obj) => {
                 let { index, name, text } = obj;
@@ -47,92 +45,6 @@ export default function Home() {
             </ContentSwitcher>
           </div>
         </div>
-        
-        
-        {/* <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a> */}
       </div>
     </main>
 
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,14 @@
 import styles from "../app/page.module.css"
 import Profile from "./Profile";
+import { FluidForm, TextInput } from "@carbon/react";
 const Sidebar = () => {
     return ( 
         <div>
             <h1>Support</h1>
             <Profile />
+            <FluidForm>
+                <TextInput type="text" labelText="search..." id="text-input-1" />
+            </FluidForm>
         </div>
      );
 }
